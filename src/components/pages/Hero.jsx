e026d91d98e62f351dad01dda3994c575531aaeb
Ali Cangül <cangulali1@gmail.com>
2023-01-29 01:11:04 +0300

    hero fixed

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-
+const Links = ["Anasayfa", "Oda Türleri", "Galeri", "İletişim"];
 const Hero = () => {
   return (
     <div className=" w-full flex justify-center my-5">
       <div className="flex justify-between max-w-7xl min-w-[70%]">
         <p className="text-3xl">otelfiyat.com</p>
         <div className="flex space-x-4 items-center">
-          <p>Anasayfa</p>
-          <p>Oda Türleri</p>
-          <p>Galeri</p>
-          <p>İletişim</p>
+          {Links.map((x, index) => (
+            <p key={index}>{x}</p>
+          ))}
         </div>
       </div>
     </div>
