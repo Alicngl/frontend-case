2f523ae2ab8968c3d516b9f616c428c1af8bf20e
Ali Cangül <cangulali1@gmail.com>
2023-01-29 14:54:20 +0300

    hero desgin fixed

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,19 +1,55 @@
 import React from "react";
-const Links = ["Anasayfa", "Oda Türleri", "Galeri", "İletişim"];
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from "@material-tailwind/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Links = ["Oda Türleri", "Galeri", "İletişim"];
 const Hero = () => {
   return (
-    <div className=" w-full flex justify-center my-5">
-      <div className="flex justify-between max-w-7xl min-w-[70%]">
+    <div className=" w-full justify-between flex   p-5 border-b-2 border-solid">
+      <div className="flex  space-x-4 items-center">
         <a href="/">
-          <p className="text-3xl">otelfiyat.com</p>
+          <p className="text-2xl">otelfiyat</p>
         </a>
         <div className="flex space-x-4 items-center">
           {Links.map((x, index) => (
             <a href="/">
-              <p key={index}>{x}</p>
+              <p key={index} className="text-sm">
+                {x}
+              </p>
             </a>
           ))}
         </div>
+      </div>
+      <div className="flex items-center space-x-3 hidden md:flex">
+        <p>Yardım</p>
+        <p>Acenteler</p>
+        <button className="border-2 p-2 border-solid rounded-md">
+          Giriş Yap
+        </button>
+      </div>
+      <div className="md:hidden items-center ">
+        <Menu
+          animate={{
+            mount: { y: 0 },
+            unmount: { y: 25 },
+          }}>
+          <MenuHandler>
+            <IconButton className="bg-white shadow-none">
+              <GiHamburgerMenu color="#333" width={800} height={40} />
+            </IconButton>
+          </MenuHandler>
+          <MenuList>
+            <MenuItem>Yardım</MenuItem>
+            <MenuItem>Acenteler</MenuItem>
+            <MenuItem>Giriş Yap</MenuItem>
+          </MenuList>
+        </Menu>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,12 @@ import Slider from "../components/pages/Slider";
 const Home = () => {
   return (
     <div className="space-y-4">
-      <div className="bg-indigo-500">
+      {/* <div className="bg-indigo-500">
         <FormControl />
       </div>
       <Hotels />
       <Slider />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
